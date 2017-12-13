030f502fe41a16b7abf1ac5aee301ddc22840ca9
Louis Dumont <dumont.louis21@gmail.com>
2017-12-13 18:59:53 +0100

    Final Version

--- isotopo.cpp
+++ isotopo.cpp
@@ -8,6 +8,7 @@
 #include "town.h"
 #include <map>
 #include <set>
+#include <algorithm>
 
 
 using namespace std;
@@ -21,7 +22,7 @@ string townFile = srcPath "/villes.txt";
 
 
 // Fonction renvoyant sous forme de texte l'histogramme des répétitions de noms de villes
-void namesRepDiag(vector<Town> towns){
+map<int,int> namesRepDiag(vector<Town> towns){
     map<string,int> name_Repetitions; //stocke pour chaque nom de ville le nombre de répétitions
     map<int,int>   histo_Repetitions; //stocke l'histogramme
     // Un parcours de towns trouve pour chaque nom le nombre de villes le portant
@@ -50,6 +51,7 @@ void namesRepDiag(vector<Town> towns){
     for (map<int,int>::iterator it=histo_Repetitions.begin(); it != histo_Repetitions.end(); it++){
         cout<<"Nombre de noms portés "<<it->first<<" fois:  "<<it->second<<endl;
     }
+    return histo_Repetitions;
 }
 
 // Returns the set of all the towns which have an homonyme
@@ -77,7 +79,7 @@ set<Town> villes_homonymes(vector<Town> towns){
 }
 
 
-void posRepDiag(vector<Town> towns){
+map<int,int> posRepDiag(vector<Town> towns){
     map<pair<float,float>,int> pos_Repetitions; //stocke pour chaque position le nombre de villes
     map<int,int>             histo_Repetitions; //stocke l'histogramme
     // Un parcours de town permet de remplir pos_Repetitions
@@ -108,6 +110,7 @@ void posRepDiag(vector<Town> towns){
     for (map<int,int>::iterator it=histo_Repetitions.begin(); it != histo_Repetitions.end(); it++){
         cout<<"Nombre de positions couvrant "<<it->first<<" villes:  "<<it->second<<endl;
     }
+    return histo_Repetitions;
 }
 
 
@@ -137,6 +140,41 @@ set<Town> villes_MultipAtPos(vector<Town> towns){
 }
 
 
+// Calcule toutes les situations trompeuses (Questions 4)
+set<Town> getPieges(vector<Town> towns){
+    // Creating all the usefull data sets
+    set<Town> pieges; //Stocking the solutions
+    set<Town> villesNomMulti = villes_homonymes(towns);
+    set<Town> villesPosMulti = villes_MultipAtPos(towns);
+    vector<Town> intersection(min(villesNomMulti.size(),villesPosMulti.size()));
+    vector<Town>::iterator inter =
+            set_intersection(villesNomMulti.begin(), villesNomMulti.end(),
+                             villesPosMulti.begin(), villesPosMulti.end(),
+                             intersection.begin());
+    intersection.resize(inter - intersection.begin());
+    // We have now discarded all the unusefull towns
+    // We examinate all the cities of inter at same pos
+    for (vector<Town>::iterator it=intersection.begin(); it!=intersection.end(); it++){
+        for(vector<Town>::iterator that=it+1; that!=intersection.end(); that++){
+            if (((*it).x() == (*that).x()) && ((*it).y() == (*that).y())){
+                // When we find two such cities, we look for to others at same pos, with corresponding names
+                for (vector<Town>::iterator s1= it+1; s1!=intersection.end(); s1++){
+                    for(vector<Town>::iterator s2= that+1; s2!=intersection.end(); s2++){
+                        bool cond1= (*s1).name() == (*it).name();
+                        bool cond2= (*s2).name() == (*that).name();
+                        bool condPos= ((*s1).x() == (*s2).x()) && ((*s1).y() == (*s2).y());
+                        if((cond1 && cond2) && condPos){
+                            pieges.insert(*it); pieges.insert(*that);
+                            pieges.insert(*s1); pieges.insert(*s2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return pieges;
+}
+
 /*
  * Study French isotoponyms
  */
@@ -161,20 +199,36 @@ int main()
 	cout << "No town found" << endl;
 	return 1;
     }
-    namesRepDiag(towns);
-    posRepDiag(towns);
+
+    // Test des histogrammes (questions 1 et 2)
+    map<int,int> question1 = namesRepDiag(towns);
+    map<int,int> question2 = posRepDiag(towns);
+
+    // Calcul de N, C, et de leur intersection
     set<Town> homonymesSet = villes_homonymes(towns);
-    int compte_Repet=0;
-    for (set<Town>::iterator it=homonymesSet.begin(); it != homonymesSet.end(); it++){
-        compte_Repet += 1;
-    }
-    cout<<"Nb de villes avec homonymes: "<<compte_Repet<<endl;
     set<Town> multiAtPosSet= villes_MultipAtPos(towns);
-    int compte_Repet2=0;
-    for (set<Town>::iterator it=multiAtPosSet.begin(); it != multiAtPosSet.end(); it++){
-        compte_Repet2 += 1;
+    vector<Town> intersection(min(homonymesSet.size(),multiAtPosSet.size()));
+    vector<Town>::iterator inter =
+            set_intersection(homonymesSet.begin(),homonymesSet.end(),
+                             multiAtPosSet.begin(), multiAtPosSet.end(),
+                             intersection.begin());
+    intersection.resize(inter - intersection.begin());
+    int interSize=0;
+    for (vector<Town>::iterator it=intersection.begin(); it != intersection.end(); it++){
+        interSize += 1;
+        cout<<*it<<endl;
     }
-    cout<<"Nb de villes avec voisines immediates: "<<compte_Repet2<<endl;
+    cout<<"L'intersection de N et C contient "<<interSize<<" elements"<<endl;
+
+    // Question 4 et affichage de son résultat
+    set<Town> P = getPieges(towns);
+    int comptePieges=0;
+    for (set<Town>::iterator it=P.begin(); it!=P.end(); it++){
+        comptePieges += 1;
+        cout<<*it<<endl;
+    }
+    cout<<"Nombre de villes dont l'association est piegeuse: "<<comptePieges<<endl;
+
     // Initialize random seed
     srand(time(NULL));
     // Say how many towns have been read
